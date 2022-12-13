bf54c5db1ef47092ed76a4097b05fb1b142d4de5
xzzit <897177017@qq.com>
2022-12-13 21:02:20 +0900

    set basic components apart for simplicity

---
+++ models/basic.py
@@ -0,0 +1,83 @@
+import torch
+
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+
+class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = out + residual
+        return out
+
+
+class UpsampleConvLayer(torch.nn.Module):
+    """UpsampleConvLayer
+    Upsamples the input and then does a convolution. This method gives better results
+    compared to ConvTranspose2d.
+    ref: http://distill.pub/2016/deconv-checkerboard/
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super().__init__()
+        self.upsample = upsample
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        x_in = x
+        if self.upsample:
+            x_in = torch.nn.functional.interpolate(x_in, mode='nearest', scale_factor=self.upsample)
+        out = self.reflection_pad(x_in)
+        out = self.conv2d(out)
+        return out
+
+
+class SelfAttention(torch.nn.Module):
+
+    def __init__(self, channels):
+        super().__init__()
+        self.query = self.conv(channels, channels // 8)
+        self.key = self.conv(channels, channels // 8)
+        self.value = self.conv(channels, channels)
+        self.gamma = torch.nn.Parameter(torch.tensor([0.]))
+        self.sm = torch.nn.Softmax(dim=1)
+
+    def conv(self, n_in, n_out):
+        return torch.nn.Conv1d(n_in, n_out, 1, bias=False)
+
+    def forward(self, x):
+        shape = x.size()
+        x = x.view(*shape[:2], -1)
+        f = self.query(x)
+        g = self.key(x)
+        h = self.value(x)
+        beta = self.sm(torch.bmm(f.transpose(1, 2), g))
+        o = self.gamma * torch.bmm(h, beta) + x
+        return o.view(*shape).contiguous()
