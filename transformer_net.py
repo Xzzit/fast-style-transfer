793d474f5775f005ba48b88adabc65ccfca1b978
xzzit <897177017@qq.com>
2022-12-13 17:53:53 +0900

    add self-attention mechanism to generator

--- transformer_net.py
+++ transformer_net.py
@@ -41,9 +41,54 @@ class TransformerNet(torch.nn.Module):
         return y
 
 
+class TransformerNet_Atten(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+        # Up-sampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Non-linearity
+        self.relu = torch.nn.ReLU()
+        # Self Attention Layers
+        self.sa2 = SelfAttention(64)
+        self.sa3 = SelfAttention(128)
+
+    def forward(self, x):  # (b, 3, h, w)
+        y = self.relu(self.in1(self.conv1(x)))  # (b, 32, h, w)
+        y = self.relu(self.in2(self.conv2(y)))  # (b, 64, h//2, w//2)
+        y = self.sa2(y)
+        y = self.relu(self.in3(self.conv3(y)))  # (b, 128, h//4, w//4)
+        y = self.sa3(y)
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))  # (b, 64, h//4, w//4)
+        y = self.relu(self.in5(self.deconv2(y)))  # (b, 32, h//2, w//2)
+        y = self.deconv3(y)  # (b, 3, h, w)
+        return y
+
+
 class ConvLayer(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super(ConvLayer, self).__init__()
+        super().__init__()
         reflection_padding = kernel_size // 2
         self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
         self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
@@ -61,7 +106,7 @@ class ResidualBlock(torch.nn.Module):
     """
 
     def __init__(self, channels):
-        super(ResidualBlock, self).__init__()
+        super().__init__()
         self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
         self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
         self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
@@ -84,7 +129,7 @@ class UpsampleConvLayer(torch.nn.Module):
     """
 
     def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
-        super(UpsampleConvLayer, self).__init__()
+        super().__init__()
         self.upsample = upsample
         reflection_padding = kernel_size // 2
         self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
@@ -99,7 +144,37 @@ class UpsampleConvLayer(torch.nn.Module):
         return out
 
 
+class SelfAttention(torch.nn.Module):
+
+    def __init__(self, channels):
+        super().__init__()
+        self.query = self.conv(channels, channels // 8)
+        self.key = self.conv(channels, channels // 8)
+        self.value = self.conv(channels, channels)
+        self.gamma = torch.nn.Parameter(torch.tensor([0.]))
+        self.sm = torch.nn.Softmax(dim=1)
+
+    def conv(self, n_in, n_out):
+        return torch.nn.Conv1d(n_in, n_out, 1, bias=False)
+
+    def forward(self, x):
+        shape = x.size()
+        x = x.view(*shape[:2], -1)
+        f = self.query(x)
+        g = self.key(x)
+        h = self.value(x)
+        beta = self.sm(torch.bmm(f.transpose(1, 2), g))
+        o = self.gamma * torch.bmm(h, beta) + x
+        return o.view(*shape).contiguous()
+
+
 if __name__ == "__main__":
     test_data = torch.randn(5, 3, 256, 256)
+
     trans = TransformerNet()
-    y = trans(test_data)
+    print(sum(p.numel() for p in trans.parameters()))
+
+    trans_attention = TransformerNet_Atten()
+    print(sum(p.numel() for p in trans_attention.parameters()))
+
+    trans_attention(test_data)
