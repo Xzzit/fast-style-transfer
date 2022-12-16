4a1f4121fe4ec381642dd9869fd6838dbc1d7b1c
Xiang Zhizheng <50263991+Xzzit@users.noreply.github.com>
2022-12-16 21:52:56 +0900

    Update stylize.py
    fix minor bug

--- stylize.py
+++ stylize.py
@@ -13,7 +13,7 @@ from models.bottleNet import BottleNetwork
 
 def stylize(content_image, model, output_image, name):
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cup')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     content_image = utils.load_image(content_image)
     content_transform = transforms.Compose([
