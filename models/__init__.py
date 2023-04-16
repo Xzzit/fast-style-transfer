11883e76ce864f4a441641376261277eecdb3791
Xzzit <897177017@qq.com>
2023-04-16 18:26:37 +0900

    fix small input bug

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from models.autoencoder import Autoencoder
 from models.autoencoder_attention import AutoencoderAttention
-from models.resNext import ResNext
+from models.bottleNet import BottleNetwork
 from models.denseNet import DenseNet
 from models.resNext import ResNext
